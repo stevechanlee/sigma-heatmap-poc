66af38f9e79f3dda385c3dbaba3f30c90a8f5c9a
Steve Lee <steve.lee@auditboard.com>
2025-09-30 12:55:36 -0700

    Use actual heatmap PNG image file for exact background match

--- src/main.js
+++ src/main.js
@@ -43,7 +43,7 @@ function renderBubbleHeatmap(data) {
     .style("left", margin.left + "px")
     .style("width", (width - margin.left - margin.right) + "px")
     .style("height", (height - margin.top - margin.bottom) + "px")
-    .style("background-image", "url('/heatmap-bg.svg')")
+    .style("background-image", "url('/Heatmap-bg.png')")
     .style("background-size", "cover")
     .style("background-position", "center")
     .style("background-repeat", "no-repeat")
